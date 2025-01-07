a06fe2e1825154c54bdd8bc6c15907ab98fd642a
Keith Simmons <ksimmons5@ncstudents.niagaracollege.ca>
2025-01-07 17:46:05 -0500

    Enemy AI
    EnemyController Script Created
    Enemy Move and Manager Scripts Deleted
    FSM Script Done
    FSMState Script Done
    AdvancedFSM Script Done

---
+++ ISO_ARPG/Assets/Prototyping/Scripts/Systems/AI Systems/FSM.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class FSM : MonoBehaviour
+{
+    //Player Transform
+    protected Transform playerTransform;
+
+    // Destination Position
+    protected Vector3 destPos;
+
+    protected virtual void Initialize() { }
+    protected virtual void FSMUpdate() { }
+    protected virtual void FSMFixedUpdate() { }
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Initialize();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FSMUpdate();
+    }
+    private void FixedUpdate()
+    {
+        FSMFixedUpdate();
+    }
+}
