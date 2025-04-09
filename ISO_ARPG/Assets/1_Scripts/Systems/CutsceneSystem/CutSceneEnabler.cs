8cfc03b3320fb9de69c99427d0bc8094e9d94e63
RivDrakos <158103191+RivDrakos@users.noreply.github.com>
2025-04-09 13:25:54 -0400

    Cutscene Change script added

--- ISO_ARPG/Assets/1_Scripts/Systems/CutsceneSystem/CutSceneEnabler.cs
+++ ISO_ARPG/Assets/1_Scripts/Systems/CutsceneSystem/CutSceneEnabler.cs
@@ -4,14 +4,28 @@ using UnityEngine;
 
 public class CutSceneEnabler : MonoBehaviour
 {
-    GameObject BerserkerCutscene;
-    GameObject HunterCutscene;
-    GameObject ElementalistCutscene;
+    [SerializeField]
+   public GameObject BerserkerCutscene;
+    [SerializeField]
+   public GameObject HunterCutscene;
+    [SerializeField]
+   public GameObject ElementalistCutscene;
     // Start is called before the first frame update
     void Start()
     {
         
      // Check current class
+     GoX_Class currentClass = PlayerManager.Instance.currentClass;
+        switch (currentClass)
+        {
+            case GoX_Class.BERSERKER:
+                BerserkerCutscene.SetActive(true); break;
+               
+            case GoX_Class.HUNTER:
+               HunterCutscene.SetActive(true); break;
+            case GoX_Class.ELEMENTALIST:
+                ElementalistCutscene.SetActive(true); break;
+        }        
      // Depending on the class enable that Cutscene
         
     }
