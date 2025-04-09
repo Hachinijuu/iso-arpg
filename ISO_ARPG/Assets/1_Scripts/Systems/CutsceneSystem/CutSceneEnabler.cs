dcc7c789ba683122272b43e87d260ea008a417f2
Keith Simmons <158103191+RivDrakos@users.noreply.github.com>
2025-04-09 01:54:57 -0400

    Cutscene Updates / Minimap

---
+++ ISO_ARPG/Assets/1_Scripts/Systems/CutsceneSystem/CutSceneEnabler.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutSceneEnabler : MonoBehaviour
+{
+    GameObject BerserkerCutscene;
+    GameObject HunterCutscene;
+    GameObject ElementalistCutscene;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+     // Check current class
+     // Depending on the class enable that Cutscene
+        
+    }
+    
+
+   
+}
